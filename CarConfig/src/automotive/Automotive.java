de520930416a190fc56cc60c8bb92835437baf5c
ylang <ylang@andrew.cmu.edu>
2013-02-06 23:59:26 -0500

    add controllor

--- CarConfig/src/automotive/Automotive.java
+++ CarConfig/src/automotive/Automotive.java
@@ -1,13 +1,27 @@
 package automotive;
 
+import java.util.ArrayList;
+
 public class Automotive {
-	private OptionSet options;
+	private ArrayList<OptionSet> options;
+	private int basePrice;
+	private String name;
 	
-	public Automotive(OptionSet options) {
+	public Automotive(int basePrice, String name, ArrayList<OptionSet> options) {
+		this.basePrice = basePrice;
+		this.name = name;
 		this.options = options;
 	}
 	
-	public OptionSet getOptions() {
+	public int basePrice() {
+		return this.basePrice;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public ArrayList<OptionSet> getOptions() {
 		return this.options;
 	}
 	
---
+++ CarConfig/src/automotive/Controller.java
@@ -0,0 +1,33 @@
+package automotive;
+
+public class Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// set colors
+		OptionSet colors = new OptionSet("Color", 10);
+		colors.setOption(0, "Fort Knox Gold Clearcoat Metallic", 0);
+		colors.setOption(1, "Liquid Grey Clearcoat Metallic", 0);
+		colors.setOption(2, "Infra-Red Clearcoat", 0);
+		colors.setOption(3, "Grabber Green Clearcoat Metallic", 0);
+		colors.setOption(4, "Sangria Red Clearcoat Metallic", 0);
+		colors.setOption(5, "French Blue Clearcoat Metallic", 0);
+		colors.setOption(6, "Twilight Blue Clearcoat Metallic", 0);
+		colors.setOption(7, "CD Silver Clearcoat Metallic", 0);
+		colors.setOption(8, "Pitch Black Clearcoat", 0);
+		colors.setOption(9, "Cloud 9 White Clearcoat", 0);
+		
+		// set transmission
+		OptionSet transmissions = new OptionSet("Transmission", 2);
+		transmissions.setOption(0, "automatic", 0);
+		transmissions.setOption(1, "manual", -815);
+		
+		
+		
+		
+		
+	}
+
+}

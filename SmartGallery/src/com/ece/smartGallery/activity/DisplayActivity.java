0d753cb31e114f55183f3fcac434e53a1ab67820
ylang <ylang@andrew.cmu.edu>
2013-04-08 02:52:34 -0400

    project 2

---
+++ SmartGallery/src/com/ece/smartGallery/activity/DisplayActivity.java
@@ -0,0 +1,22 @@
+package com.ece.smartGallery.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DisplayActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display, menu);
+		return true;
+	}
+
+}

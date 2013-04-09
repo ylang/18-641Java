b2bb1c2d7baf4920a502f5a7d3c90110a20c6ee8
liusizhe <liusizhevincent@gmail.com>
2013-04-09 18:27:19 -0400

    added folders and packages for step 4 and 5, added sample interface for step 5, still not sure what to write for step 4

---
+++ SmartGallery/src/com/ece/smartGallery/ws/remote/Remote.java
@@ -0,0 +1,14 @@
+package com.ece.smartGallery.ws.remote;
+
+import android.content.Intent;
+
+public interface Remote {
+	public void onCreate();
+	
+	public void onDestry();
+	
+	public void onStart(Intent intent);
+	
+
+	
+}

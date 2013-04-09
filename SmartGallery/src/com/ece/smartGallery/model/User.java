8dc385ee18ae3477bbd3e85718c40e14b1a00bc1
Ruoyu Li <lllrrrryyy@gmail.com>
2013-04-09 15:48:42 -0400

    add databeans

---
+++ SmartGallery/src/com/ece/smartGallery/model/User.java
@@ -0,0 +1,41 @@
+package com.ece.smartGallery.model;
+
+public class User {
+
+	private int Id;
+	private String firstName;
+	private String lastName;
+	private Album[] albums;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Album[] getAlbums() {
+		return albums;
+	}
+
+	public void setAlbums(Album[] albums) {
+		this.albums = albums;
+	}
+}

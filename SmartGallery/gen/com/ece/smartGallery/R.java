b3377221d8df621476acdfd76c2f9e689244d0e0
liusizhe <liusizhevincent@gmail.com>
2013-04-16 23:33:38 -0400

    added the display and edit xml files and add one icon

--- SmartGallery/gen/com/ece/smartGallery/R.java
+++ SmartGallery/gen/com/ece/smartGallery/R.java
@@ -25,21 +25,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
+        public static final int add_scratch_button=0x7f080009;
         public static final int add_voice_button=0x7f080008;
-        public static final int display_comment=0x7f080002;
         public static final int display_geolocation=0x7f080001;
         public static final int display_image=0x7f080000;
-        public static final int draw_view=0x7f080009;
-        public static final int edit_cancel=0x7f08000b;
+        public static final int display_text_comment=0x7f080002;
+        public static final int draw_view=0x7f08000a;
+        public static final int edit_cancel=0x7f08000c;
         public static final int edit_comment_input=0x7f080007;
-        public static final int edit_geolocation=0x7f080006;
-        public static final int edit_image=0x7f080005;
-        public static final int edit_save=0x7f08000a;
-        public static final int gallery_list=0x7f08000d;
-        public static final int imageButton1=0x7f080003;
-        public static final int imageButton2=0x7f080004;
-        public static final int imageView1=0x7f08000c;
+        public static final int edit_image=0x7f080006;
+        public static final int edit_save=0x7f08000b;
+        public static final int gallery_list=0x7f08000e;
+        public static final int imageButton1=0x7f080004;
+        public static final int imageButton2=0x7f080005;
+        public static final int imageView1=0x7f08000d;
+        public static final int play_voice_comment=0x7f080003;
     }
     public static final class layout {
         public static final int activity_display=0x7f030000;
@@ -54,18 +55,26 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int add=0x7f050003;
-        public static final int add_comment_here=0x7f05000b;
+        public static final int add_comment_here=0x7f050013;
         public static final int add_picture=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int back=0x7f05000a;
-        public static final int cancel=0x7f05000d;
+        public static final int back=0x7f050012;
+        public static final int cancel=0x7f050015;
+        public static final int default_geolocation=0x7f05000a;
         public static final int default_picture=0x7f050007;
         public static final int default_text=0x7f050008;
+        public static final int default_text_comment=0x7f050009;
+        public static final int edit_scratch_comment=0x7f05000d;
+        public static final int edit_text_comment=0x7f05000c;
         public static final int hello_world=0x7f050002;
-        public static final int option=0x7f050009;
-        public static final int save=0x7f05000c;
+        public static final int option=0x7f050011;
+        public static final int play_voice_comment=0x7f05000f;
+        public static final int save=0x7f050014;
+        public static final int scratch_comment=0x7f050010;
+        public static final int text_comment=0x7f05000b;
         public static final int title_activity_display=0x7f050005;
         public static final int title_activity_edit=0x7f050006;
+        public static final int voice_comment=0x7f05000e;
     }
     public static final class style {
         /** 

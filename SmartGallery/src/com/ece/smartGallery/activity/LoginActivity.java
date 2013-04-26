d482ff6ccaa2ec29811ba1f5f455eb635eb58526
Ruoyu Li <lllrrrryyy@gmail.com>
2013-04-26 03:07:00 -0400

    login with button and ui control

--- SmartGallery/src/com/ece/smartGallery/activity/LoginActivity.java
+++ SmartGallery/src/com/ece/smartGallery/activity/LoginActivity.java
@@ -1,52 +1,35 @@
 package com.ece.smartGallery.activity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 
 import com.ece.smartGallery.R;
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
+
+	private LoginFragment loginFragment;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
 
-		Session.openActiveSession(this, true, new Session.StatusCallback() {
-			@Override
-			public void call(Session session, SessionState state,
-					Exception exception) {
-				if (session.isOpened()) {
-					// make request to the /me API
-					Request.executeMeRequestAsync(session,
-							new Request.GraphUserCallback() {
-								// callback after Graph API response with user
-								@Override
-								public void onCompleted(GraphUser user,
-										Response response) {
-									if (user != null) {
-										TextView welcome = (TextView) findViewById(R.id.welcome);
-										welcome.setText("Hello "
-												+ user.getName() + "!");
-									}
-								}
-							});
-				}
-			}
-		});
+		if (savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			loginFragment = new LoginFragment();
+			getSupportFragmentManager().beginTransaction()
+					.add(android.R.id.content, loginFragment).commit();
+		} else {
+			// Or set the fragment from restored state info
+			loginFragment = (LoginFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
 	}
 
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode,
-				resultCode, data);
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display, menu);
+		return true;
 	}
 }

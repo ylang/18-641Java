2ffcb6b0a392ba39bf25848455a0a976600ab2f2
Ruoyu Li <lllrrrryyy@gmail.com>
2013-04-27 04:35:32 -0400

    what

--- SmartGallery/gen/com/ece/smartGallery/R.java
+++ SmartGallery/gen/com/ece/smartGallery/R.java
@@ -212,7 +212,8 @@ containing a value of this type.
         public static final int com_facebook_top_background=0x7f02001d;
         public static final int com_facebook_top_button=0x7f02001e;
         public static final int com_facebook_usersettingsfragment_background_gradient=0x7f02001f;
-        public static final int ic_launcher=0x7f020020;
+        public static final int gallery=0x7f020020;
+        public static final int ic_launcher=0x7f020021;
     }
     public static final class id {
         public static final int action_settings=0x7f04003f;

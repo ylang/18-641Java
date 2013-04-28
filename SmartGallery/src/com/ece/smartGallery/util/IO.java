3594bb2bc86581ca789153d45c951752be9d059f
ylang <ylang@andrew.cmu.edu>
2013-04-28 05:27:43 -0400

    make transforablephoto

--- SmartGallery/src/com/ece/smartGallery/util/IO.java
+++ SmartGallery/src/com/ece/smartGallery/util/IO.java
@@ -1,8 +1,13 @@
 package com.ece.smartGallery.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectOutputStream;
+
+import android.util.Base64;
 
 public class IO {
 	public static byte[] getByteArray(java.io.Serializable s)
@@ -21,5 +26,41 @@ public class IO {
 			bos.close();
 		}
 		return bytes;
+	}
+	
+//	private String encodeFileToBase64Binary(String fileName)
+//			throws IOException {
+// 
+//		File file = new File(fileName);
+//		byte[] bytes = loadFile(file);
+//		byte[] encoded = Base64.encodeBase64(bytes);
+//		String encodedString = new String(encoded);
+// 
+//		return encodedString;
+//	}
+ 
+	public static byte[] loadFile(File file) throws IOException {
+	    InputStream is = new FileInputStream(file);
+ 
+	    long length = file.length();
+	    if (length > Integer.MAX_VALUE) {
+	        // File is too large
+	    }
+	    byte[] bytes = new byte[(int)length];
+	    
+	    int offset = 0;
+	    int numRead = 0;
+	    while (offset < bytes.length
+	           && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+	        offset += numRead;
+	    }
+ 
+	    if (offset < bytes.length) {
+	    	is.close();
+	        throw new IOException("Could not completely read file "+file.getName());
+	    }
+ 
+	    is.close();
+	    return bytes;
 	}
 }
--- SmartGallery/src/com/ece/smartGallery/util/TransforablePhoto.java
+++ SmartGallery/src/com/ece/smartGallery/util/TransforablePhoto.java
@@ -1,6 +1,10 @@
 package com.ece.smartGallery.util;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+
+import android.content.Context;
 
 import com.ece.smartGallery.DBLayout.Photo;
 
@@ -9,10 +13,43 @@ public class TransforablePhoto implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -6090493483762339387L;
-	private Photo photo;
+	private String name;
+	private long timeStamp;
+	private String text;	//text comment
 	
-	public TransforablePhoto(Photo photo) {
-		this.photo = photo;
-	}
+	private byte[] imageBytes;
+	private byte[] voiceBytes;
+	private byte[] scratchBytes;
 
+	private int commentType;
+	private String location;
+	private double lng, lat;
+	
+	public TransforablePhoto(Context context, Photo photo) {
+		this.name = photo.getName();
+		this.timeStamp = photo.getTimeStamp();
+		this.text = photo.getText();
+		this.location = photo.getLocation();
+		this.lat = photo.getLat();
+		this.lng = photo.getLng();
+		try {
+			this.imageBytes = IO.loadFile(new File (photo.getImage().getPath()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (photo.getVoice() != null && photo.getVoice().length() != 0) {
+			try {
+				this.voiceBytes = IO.loadFile(new File (photo.getVoice()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (photo.getScratchURI() != null && photo.getScratchURI().getPath().length() != 0) {
+			try {
+				this.voiceBytes = IO.loadFile(new File (photo.getScratchURI().getPath()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
